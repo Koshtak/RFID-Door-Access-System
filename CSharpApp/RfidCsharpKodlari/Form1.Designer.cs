17c78ed0184a039cad49fd0b4700477166f82e17
Koshtak <lorerime101@gmail.com>
2025-08-07 15:52:46 +0300

    butonlar

--- CSharpApp/RfidCsharpKodlari/Form1.Designer.cs
+++ CSharpApp/RfidCsharpKodlari/Form1.Designer.cs
@@ -30,42 +30,17 @@
         {
             this.components = new System.ComponentModel.Container();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
-            this.txtLog = new System.Windows.Forms.TextBox();
-            this.dataGridViewCards = new System.Windows.Forms.DataGridView();
-            this.txtNewUID = new System.Windows.Forms.TextBox();
             this.btnAddUID = new System.Windows.Forms.Button();
             this.btnRemoveUID = new System.Windows.Forms.Button();
+            this.dataGridViewCards = new System.Windows.Forms.DataGridView();
+            this.txtNewUID = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCards)).BeginInit();
             this.SuspendLayout();
             // 
             // serialPort1
             // 
             this.serialPort1.PortName = "COM7";
-            // 
-            // txtLog
-            // 
-            this.txtLog.Location = new System.Drawing.Point(122, 63);
-            this.txtLog.Multiline = true;
-            this.txtLog.Name = "txtLog";
-            this.txtLog.ReadOnly = true;
-            this.txtLog.Size = new System.Drawing.Size(100, 20);
-            this.txtLog.TabIndex = 0;
-            // 
-            // dataGridViewCards
-            // 
-            this.dataGridViewCards.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewCards.Location = new System.Drawing.Point(241, 102);
-            this.dataGridViewCards.Name = "dataGridViewCards";
-            this.dataGridViewCards.Size = new System.Drawing.Size(240, 150);
-            this.dataGridViewCards.TabIndex = 1;
-            // 
-            // txtNewUID
-            // 
-            this.txtNewUID.Location = new System.Drawing.Point(149, 277);
-            this.txtNewUID.Name = "txtNewUID";
-            this.txtNewUID.Size = new System.Drawing.Size(100, 20);
-            this.txtNewUID.TabIndex = 2;
-            this.txtNewUID.Text = "write here";
             // 
             // btnAddUID
             // 
@@ -75,26 +50,53 @@
             this.btnAddUID.TabIndex = 3;
             this.btnAddUID.Text = "add";
             this.btnAddUID.UseVisualStyleBackColor = true;
+            this.btnAddUID.Click += new System.EventHandler(this.btnAddUID_Click);
             // 
             // btnRemoveUID
             // 
-            this.btnRemoveUID.Location = new System.Drawing.Point(558, 291);
+            this.btnRemoveUID.Location = new System.Drawing.Point(561, 293);
             this.btnRemoveUID.Name = "btnRemoveUID";
             this.btnRemoveUID.Size = new System.Drawing.Size(75, 23);
             this.btnRemoveUID.TabIndex = 4;
             this.btnRemoveUID.Text = "remove";
             this.btnRemoveUID.UseVisualStyleBackColor = true;
+            this.btnRemoveUID.Click += new System.EventHandler(this.btnRemoveUID_Click);
+            // 
+            // dataGridViewCards
+            // 
+            this.dataGridViewCards.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCards.Location = new System.Drawing.Point(307, 121);
+            this.dataGridViewCards.Name = "dataGridViewCards";
+            this.dataGridViewCards.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewCards.TabIndex = 5;
+            // 
+            // txtNewUID
+            // 
+            this.txtNewUID.Location = new System.Drawing.Point(106, 295);
+            this.txtNewUID.Name = "txtNewUID";
+            this.txtNewUID.Size = new System.Drawing.Size(100, 20);
+            this.txtNewUID.TabIndex = 6;
+            this.txtNewUID.Text = "Write Here";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(106, 31);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.Size = new System.Drawing.Size(100, 20);
+            this.txtLog.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnRemoveUID);
-            this.Controls.Add(this.btnAddUID);
+            this.Controls.Add(this.txtLog);
             this.Controls.Add(this.txtNewUID);
             this.Controls.Add(this.dataGridViewCards);
-            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnRemoveUID);
+            this.Controls.Add(this.btnAddUID);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -107,11 +109,11 @@
         #endregion
 
         private System.IO.Ports.SerialPort serialPort1;
-        private System.Windows.Forms.TextBox txtLog;
-        private System.Windows.Forms.DataGridView dataGridViewCards;
-        private System.Windows.Forms.TextBox txtNewUID;
         private System.Windows.Forms.Button btnAddUID;
         private System.Windows.Forms.Button btnRemoveUID;
+        private System.Windows.Forms.DataGridView dataGridViewCards;
+        private System.Windows.Forms.TextBox txtNewUID;
+        private System.Windows.Forms.TextBox txtLog;
     }
 }
 
--- CSharpApp/RfidCsharpKodlari/Form1.cs
+++ CSharpApp/RfidCsharpKodlari/Form1.cs
@@ -15,6 +15,8 @@ namespace RfidCsharpKodlari
 {
     public partial class Form1 : Form
     {
+        private readonly string connectionString = "server=localhost;Database=rfidsystem;Trusted_Connection=True;";
+
         public Form1()
         {
             InitializeComponent();
@@ -22,35 +24,143 @@ namespace RfidCsharpKodlari
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            serialPort1.Open();
-            serialPort1.DataReceived += SerialPort1_DataReceived;
+            InitializeSerialPort();
+            LoadAuthorizedCards();
         }
 
-        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+       
+        private void InitializeSerialPort()
         {
-            string uid = serialPort1.ReadLine().Trim();//arduinodan id al, boşlukları temizle.
-            bool isAuthorized = false;
-            string connectionString = "server=localhost;Database=rfidsystem;Trusted_Connection=True;";
-
-
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            try
             {
-                conn.Open();
-
-                SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM authorizedCards WHERE CardUID=@uid", conn);
-                cmd.Parameters.AddWithValue("@uid", uid);
-
-                int count = (int)cmd.ExecuteScalar();
-                isAuthorized = (count > 0);
+                serialPort1.DataReceived += SerialPort1_DataReceived;
+                serialPort1.Open();
+                Log("Serial port open.");
+            }
+            catch (Exception ex)
+            {
+                Log("opening serial port error " + ex.Message);
             }
 
+        }
 
-            if (isAuthorized)
-                serialPort1.WriteLine("ACCESS_GRANTED");
-            else
-                serialPort1.WriteLine("ACCESS_DENIED");
+        private void LoadAuthorizedCards()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "SELECT * FROM authorizedCards";
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+
+                    System.Data.DataTable dt = new System.Data.DataTable();
+                    adapter.Fill(dt);
+                    dataGridViewCards.DataSource = dt;
+
+                    Log("Cards loaded from database.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log("Error loading cards: " + ex.Message);
+            }
+        }
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
+
+            string uid = serialPort1.ReadLine().Trim();//arduinodan id al, boşlukları temizle.
+            bool isAuthorized = CheckAuthorization(uid);
+
+            string response = isAuthorized ? "ACCESS_GRANTED" : "ACCESS_DENIED";
+            serialPort1.WriteLine(response);
+
+            Invoke(new Action(() =>{ Log($"Card Read: {uid} → {response}");}));
 
         }
 
+        private bool CheckAuthorization(string uid)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = "SELECT COUNT(*) FROM authorizedCards WHERE CardUID = @uid";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@uid", uid);
+
+                    int count = (int)cmd.ExecuteScalar();
+                    return count > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Invoke(new Action(() =>{Log("Error checking UID: " + ex.Message);}));
+                return false;
+            }
+        }
+
+        private void btnAddUID_Click(object sender, EventArgs e)
+        {
+            string newUID = txtNewUID.Text.Trim();
+            if (string.IsNullOrEmpty(newUID)) return;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = "INSERT INTO authorizedCards (CardUID) VALUES (@uid)";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@uid", newUID);
+                    cmd.ExecuteNonQuery();
+                }
+
+                Log($"Added UID: {newUID}");
+                txtNewUID.Clear();
+                LoadAuthorizedCards();
+            }
+            catch (Exception ex)
+            {
+                Log("Error adding UID: " + ex.Message);
+            }
+        }
+
+        private void btnRemoveUID_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewCards.CurrentRow == null) return;
+
+            string uidToRemove = dataGridViewCards.CurrentRow.Cells["CardUID"].Value.ToString();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = "DELETE FROM authorizedCards WHERE CardUID = @uid";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@uid", uidToRemove);
+                    cmd.ExecuteNonQuery();
+                }
+
+                Log($"Removed UID: {uidToRemove}");
+                LoadAuthorizedCards();
+            }
+            catch (Exception ex)
+            {
+                Log("Error removing UID: " + ex.Message);
+            }
+        }
+        private void Log(string message)
+        {
+            txtLog.AppendText($"{DateTime.Now}: {message}{Environment.NewLine}");
+        }
+
+       
     }
 }
